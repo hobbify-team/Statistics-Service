f034ecc96f06e4cda42f04b2ef593f7c4cded912
wdiazplus <social@wilmerdiaz.com>
2020-07-09 00:18:46 -0500

    Issue 001 completed and updated

---
+++ Hobbify-Stats/server.py
@@ -0,0 +1,32 @@
+from flask import Flask, request , jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+import os
+
+
+#Init app
+app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+#Database
+app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+#Inti db
+db = SQLAlchemy(app)
+#Init ma
+ma = Marshmallow(app)
+
+
+#Endpoint GET
+@app.route('/', methods=['GET'])
+
+def fr():
+    return jsonify({'message': 'endpoints get in use'})
+
+
+#Run server 
+
+if __name__ == '__main__':
+    app.run(debug=True)
