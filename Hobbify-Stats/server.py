c75863ad80e6e29dc9ba672e8203d1878fe3c3dc
Jonattan Infante Vargas <infanpearl@gmail.com>
2020-07-17 15:54:21 -0500

    Add Nginx and Conf to wsgi

--- Hobbify-Stats/server.py
+++ Hobbify-Stats/server.py
@@ -7,11 +7,8 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
    # Connect to an existing database
-   querys.insertScore(2,date(2002, 12, 31),'Yoiky')
+   #querys.insertScore(2,date(2002, 12, 31),'Yoiky')
    
    
    return 'hola'
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=80)
-    
